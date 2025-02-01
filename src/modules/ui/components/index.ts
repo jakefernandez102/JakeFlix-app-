7b7d9bd70ea71f1811536ce7edd3b5f6932f7a60
JakeSteve <jake.fernandezbrizuela@outlook.com>
2025-02-01 10:42:12 -0500

    opt(separate-in-modules): Adding index.ts to folders to export content

--- src/modules/ui/components/index.ts
+++ src/modules/ui/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./MovieCard/MovieCard";
 export * from "./MovieList/MovieList";
 export * from "./Top10MovieCard/Top10MovieCard";
-export * from "./animation";
+export * from "./animation/LogoAnimation";
+export * from "./Advertisement/Advertisement";
